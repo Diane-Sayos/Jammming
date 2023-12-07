0e70ef0993a6646dbdd40092253809504acf35c8
divineheart <100250310+Diane-Sayos@users.noreply.github.com>
2023-12-07 11:44:48 -0500

    Tracks added

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,8 +1,10 @@
 //will output list of songs from searchbar input
+import TrackList from '../TrackList/TrackList';
 const SearchResults = () => {
     return (
-        <section id='search-results'>
-
+        <section className='SearchResults'>
+            <h2>Results</h2>
+            <TrackList />
         </section>
     )
 }
